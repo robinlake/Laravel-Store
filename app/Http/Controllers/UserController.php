3d6fb719c5db75a20fe1b01ea829253bb2e1e753
Robin Lake <mister.lake@gmail.com>
2017-04-29 18:38:51 -0600

    Login functioning

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+
+class UserController extends Controller
+{
+    public function getSignup(){
+        return view('user.signup');
+    }
+    /*
+    * Create new user
+    */
+    public function postSignup(Request $request) {
+        $this->validate($request, [
+            'email' => 'email|required|unique:users',
+            'password' => 'required|min:4'
+        ]);
+
+        $user = new User([
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+        $user->save();
+
+        return redirect()->route('product.index');
+    }
+
+    /*
+    * User Log-in
+    */
+    public function getSignin() {
+        return view('user.signin');
+    }
+
+    public function postSignin(Request $request) {
+        $this->validate($request, [
+            'email' => 'email|required',
+            'password' => 'required|min:4'
+        ]);
+
+        if (Auth::attempt(['email' => $request->input('email'), 'password' =>
+        $request->input('password')])) {
+            return redirect()->route('user.profile');
+        }
+        return redirect()->back();
+    }
+
+    /*
+    * User Profile
+    */
+
+    public function getProfile() {
+        return view('user.profile');
+    }
+
+    public function getLogout() {
+        Auth::logout();
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,63 @@ Route::get('/', [
     'uses' => 'ProductController@getIndex',
     'as' => 'product.index'
 ]);
+
+/*
+*---------------
+*Create new user
+*---------------
+*/
+
+
+
+Route::group(['prefix' => 'user'], function(){
+    Route::group(['middleware' => 'guest'], function(){
+
+        Route::get('/signup', [
+            'uses' => 'UserController@getSignup',
+            'as' => 'user.signup'
+        ]);
+
+        Route::post('/signup', [
+            'uses' => 'UserController@postSignup',
+            'as' => 'user.signup'
+        ]);
+
+        /*
+        *---------------------
+        *Existing user sign-in
+        *---------------------
+        */
+
+        Route::get('/signin', [
+            'uses' => 'UserController@getSignin',
+            'as' => 'user.signup'
+        ]);
+
+        Route::post('/signin', [
+            'uses' => 'UserController@postSignin',
+            'as' => 'user.signin'
+        ]);
+
+    });
+
+    /*
+    * Get user profile
+    */
+    Route::group(['middleware' => 'auth'], function(){
+
+        Route::get('/profile', [
+            'uses' => 'UserController@getProfile',
+            'as' => 'user.profile'
+        ]);
+
+        Route::get('/logout',[
+            'uses' => 'UserController@getLogout',
+            'as' => 'user.logout'
+        ]);
+    });
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
